94fe1dabd02c812de5c2ccdedcdf37d87c143402
JF532 <jfilipe532@hotmail.com>
2025-07-25 03:15:44 -0300

    Mudança em alguns arquivos do FRONT para que o tailwind e flowbite funcionem corretamente

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,11 +1,12 @@
-
-import { Router } from 'react-router';
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/index";
 function App() {
   return(
     <Router>
-      
+      <Routes>
+      <Route path="/home" element={<Home />} />
+
+      </Routes>
     </Router>
 
   );
